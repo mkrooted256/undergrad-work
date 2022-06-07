5183a4d7be266cd92b8698cf58af09c627b89206
Mykhailo Koreshkov <mykhailo.koreshkov@fracturedbyte.com>
2022-06-07 05:33:04 +0300

    CG lab raytracing - initial commit

---
+++ 6-graphics/raytracing/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <windows.h>
+
+#include <vector>
+#include <cassert>
+
+#include "geom.h" 
+
+// ------------------------
+
+
+int main() {
+
+    return 0;
+}
