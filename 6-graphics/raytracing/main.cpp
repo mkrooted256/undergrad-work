f0a549d906ec35d5271dd34badc3391aa3bcd4de
Mykhailo Koreshkov <mykhailo.koreshkov@fracturedbyte.com>
2022-06-10 03:49:16 +0300

    Add example images

--- 6-graphics/raytracing/main.cpp
+++ 6-graphics/raytracing/main.cpp
@@ -9,45 +9,37 @@
 #include <cassert>
 
 #include "render.h"
+#include "AntiAliasing.h"
 
 // ------------------------
 
 void prepare_bodies(std::vector<std::shared_ptr<Body>>& bodies) 
 {
-    // Yellow center sphere
-    auto* geom = (Geometry*) new Sphere({0,0,0}, 30);
+    auto* geom = (Geometry*) new Sphere({-25,0,10}, 24);
     auto* mat = new Material();
-    mat->matte = Vec3f{ 250, 241, 53 } / 255;
+    mat->matte = Vec3f{ 1,0,0 };
     bodies.emplace_back(new Body(geom, mat));
 
-    // Blue closer right sphere
-    geom = (Geometry*) new Sphere({20, -30, 10}, 10);
+    geom = (Geometry*) new Sphere({25,0,10}, 24);
     mat = new Material();
-    mat->matte = Color{ 0.2, 0.2, 0.9 };
+    mat->matte = Color{ 1,0,0 };
+    bodies.emplace_back(new Body(geom, mat));
+
+    geom = (Geometry*) new Sphere({0,0,0}, 28);
+    mat = new Material();
+    mat->matte = Color{ 0.2,0.2,1 };
     bodies.emplace_back(new Body(geom, mat));
 }
 
 void prepare_lights(std::vector<std::shared_ptr<Light>>& lights)
 {
     auto* light = new PointLight();
-    light->loc = { 50, -100, 50 };
+    light->loc = { 50, -80, -5 };
     light->color = { 1, 1, 1 };
     light->brightness = 1;
 
     lights.emplace_back(light);
 }
-
-
-void test1() {
-    Sphere S({0,0,0}, 50);
-
-    TraceResult tr{};
-    bool hit = S.Intersect(Ray{{0,0,0}, Vec3f{0.9,0.3,0}.Ort()}, tr);
-
-    if (!hit) std::cout<<"Miss";
-    else std::cout << "hit at " << tr.hitpoint << " with n" << tr.normal << " after " << tr.t << std::endl;
-}
-
 
 int main()
 {
@@ -65,19 +57,35 @@ int main()
     Renderer renderer;
     renderer.scene = scene.get();
 
-    renderer.width = 400;
-    renderer.height = 300;
-    renderer.fov = 60;
-    renderer.camera = { {0, -100, 0}, {0, 1, 0} };
+    renderer.width = 800;
+    renderer.height = 600;
+    renderer.fov = 75;
+    renderer.camera = { {0, -120, 0}, {0, 1, 0} };
     renderer.camera_up = { 0, 0, 1 };
     renderer.camera_right = { 1, 0, 0 };
 
     renderer.backgroundColor = Color{0,0,0};
-    renderer.globalIllumination = Color{1,1,1} * 0.1;
+    renderer.globalIllumination = Color{1,1,1} * 0.05;
 
     std::cout << "Starting the render..." << std::endl;
-    renderer.Render("hellocg.bmp");
+    renderer.Render();
     std::cout << "Render finished!" << std::endl;
+
+    BMP bmp(renderer.width, renderer.height, renderer.GetBuffer());
+    bmp.write("output.bmp");
+
+//    UniformSSAAx4 ssaa(&renderer);
+//    ssaa.Apply();
+//
+//    bmp = BMP(ssaa.GetWidth(), ssaa.GetHeight(), ssaa.GetOutput());
+//    bmp.write("output-aa.bmp");
+//
+//    AverageFilter avgFilter(&renderer, 3);
+//    avgFilter.Apply();
+//
+//    bmp = BMP(avgFilter.GetWidth(), avgFilter.GetHeight(), avgFilter.GetOutput());
+//    bmp.write("output-avg3.bmp");
+
 
     return 0;
 }
